6be2ce854623c9e0be66cbe111779729e03c80f8
INnoVatioN-97 <koyoungil97@gmail.com>
2021-06-23 18:52:53 +0900

    homepage 수정

--- src/components/login/AuthForm.js
+++ src/components/login/AuthForm.js
@@ -1,51 +1,40 @@
 import React, { useState } from 'react';
-import { authService} from 'fBase';
+import { authService } from 'fBase';
 
 const AuthForm = () => {
     const [newAccount, setNewAccount] = useState(true);
-    const [error, setError] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [error, setError] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const onChange = (event) => {
-      const {
-        target: { name, value },
-      } = event;
-      if (name === "email") {
-        setEmail(value);
-      } else if (name === "password") {
-        setPassword(value);
-      }
+        const {
+            target: { name, value },
+        } = event;
+        if (name === 'email') {
+            setEmail(value);
+        } else if (name === 'password') {
+            setPassword(value);
+        }
     };
     const onSubmit = async (event) => {
-      event.preventDefault();
-      try {
-        let data;
-        if (newAccount) {
-          data = await authService.createUserWithEmailAndPassword(
-            email,
-            password
-          );
-        } else {
-          data = await authService.signInWithEmailAndPassword(email, password);
+        event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            setError(error.message);
         }
-        console.log(data);
-      } catch (error) {
-        setError(error.message);
-      }
     };
     const toggleAccount = () => setNewAccount((prev) => !prev);
     return (
         <>
             <form onSubmit={onSubmit} className="container">
-                <input
-                    name="email"
-                    type="email"
-                    placeholder="Email"
-                    required
-                    value={email}
-                    onChange={onChange}
-                    className="authInput"
-                />
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} className="authInput" />
                 <input
                     name="password"
                     type="password"
@@ -55,17 +44,13 @@ const AuthForm = () => {
                     onChange={onChange}
                     className="authInput"
                 />
-                <input
-                    type="submit"
-                    className="authInput authSubmit"
-                    value={newAccount ? "Create Account" : "Sign In"}
-                />
+                <input type="submit" className="authInput authSubmit" value={newAccount ? 'Create Account' : 'Sign In'} />
                 {error && <span className="authError">{error}</span>}
             </form>
             <span onClick={toggleAccount} className="authSwitch">
-                {newAccount ? "Sign In" : "Create Account"}
+                {newAccount ? 'Sign In' : 'Create Account'}
             </span>
         </>
-    )
+    );
 };
 export default AuthForm;
